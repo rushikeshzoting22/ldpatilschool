6fad5ed799d9219dfae955d3e06db6936524beab
MilestoneWebstudio <rzoting22@gmail.com>
2023-04-11 18:49:56 +0530

    Header Updated

--- js/custom.js
+++ js/custom.js
@@ -55,7 +55,32 @@ jQuery(document).ready(function($) {
 				}
 			});
 
-		
+		//header
+        (function($, document, window){
+	
+            $(document).ready(function(){
+        
+                // Cloning main navigation for mobile menu
+                $(".mobile-navigation").append($(".main-navigation .menu").clone());
+        
+                // Mobile menu toggle 
+                $(".menu-toggle").click(function(){
+                    $(".mobile-navigation").slideToggle();
+                });
+        
+                $(".hero").flexslider({
+                    directionNav: false,
+                    controlNav: true
+                });
+            });
+        
+            $(window).load(function(){
+        
+            });
+        
+        })(jQuery, document, window);
+//header
+
 		//scroll to top
 		$(window).scroll(function(){
 			if ($(this).scrollTop() > 100) {
